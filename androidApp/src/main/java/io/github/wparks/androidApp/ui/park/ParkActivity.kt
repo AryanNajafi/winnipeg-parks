79fe3683a4bf4b641606bdf3ce89e23c9c7b42a7
Aryan Najafi <aryannjfi@gmail.com>
2021-07-11 23:50:02 -0500

    Change park toolbar to transparent topAppBar

--- androidApp/src/main/java/io/github/wparks/androidApp/ui/park/ParkActivity.kt
+++ androidApp/src/main/java/io/github/wparks/androidApp/ui/park/ParkActivity.kt
@@ -1,7 +1,6 @@
 package io.github.wparks.androidApp.ui.park
 
 import android.os.Bundle
-import android.view.MenuItem
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
@@ -9,13 +8,26 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
 import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
 import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.ArrowBack
 import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.viewinterop.AndroidView
+import androidx.core.view.WindowCompat
+import com.google.accompanist.insets.LocalWindowInsets
+import com.google.accompanist.insets.ProvideWindowInsets
+import com.google.accompanist.insets.rememberInsetsPaddingValues
+import com.google.accompanist.insets.ui.Scaffold
+import com.google.accompanist.insets.ui.TopAppBar
+import com.google.accompanist.systemuicontroller.rememberSystemUiController
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.MapView
 import com.google.android.gms.maps.model.LatLng
@@ -35,48 +47,73 @@ class ParkActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+
         setContent {
+            val systemUiController = rememberSystemUiController()
+            val useDarkIcons = MaterialTheme.colors.isLight
+            SideEffect {
+                systemUiController.setSystemBarsColor(Color.Transparent, darkIcons = useDarkIcons)
+            }
             MyApp {
-                ParkInfo(viewModel)
+                ProvideWindowInsets {
+                    ParkInfo(viewModel, onBackPressed = { finish() })
+                }
             }
         }
 
         val parkId = intent.getLongExtra(HomeActivity.INTENT_KEY_PARK_ID, -1)
-        val parkTitle = intent.getStringExtra(HomeActivity.INTENT_KEY_PARK_TITLE)
-
-        supportActionBar?.apply {
-            setDisplayHomeAsUpEnabled(true)
-            setHomeButtonEnabled(true)
-            title = parkTitle
-        }
 
         viewModel.loadParkInfo(parkId)
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        if (item.itemId == android.R.id.home) {
-            finish()
-            return true
-        }
-        return super.onOptionsItemSelected(item)
     }
 
 }
 
 @Composable
-fun ParkInfo(viewModel: ParkViewModel) {
+fun ParkInfo(viewModel: ParkViewModel, onBackPressed: () -> Unit) {
     val viewState: ParkViewState by viewModel.uiState.collectAsState()
 
     val typography = MaterialTheme.typography
     
-    Surface(modifier = Modifier
-        .fillMaxSize()
-        .background(color = MaterialTheme.colors.surface)
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = {},
+                backgroundColor = Color.Transparent,
+                navigationIcon = {
+                    IconButton(onClick = onBackPressed) {
+                        Icon(imageVector = Icons.Rounded.ArrowBack, contentDescription = "Back")
+                    }
+                },
+                contentPadding = rememberInsetsPaddingValues(
+                    insets = LocalWindowInsets.current.statusBars,
+                    applyBottom = false
+                ),
+                elevation = 0.dp,
+                modifier = Modifier.fillMaxWidth(),
+            )
+        }
     ) {
         Column {
-            Box(modifier = Modifier.height(200.dp)) {
-                if (viewState.park != null) {
-                    ParkMapView(viewState.assets)
+            Box(modifier = Modifier.height(300.dp)) {
+                viewState.park?.let { park ->
+                    ParkMapView(viewState.assets, LatLng(park.latitude!!, park.longitude!!))
+                    Column(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .background(Color(0x5C181818))
+                            .align(Alignment.BottomEnd)
+                            .padding(horizontal = 10.dp, vertical = 5.dp)
+                    ) {
+                        Text(
+                            text = park.title,
+                            color = Color.White,
+                            style = MaterialTheme.typography.subtitle1)
+                        Text(
+                            text = park.address,
+                            color = Color.White,
+                            style = MaterialTheme.typography.subtitle2)
+                    }
                 }
             }
 
@@ -93,40 +130,55 @@ fun ParkInfo(viewModel: ParkViewModel) {
                 }
             }
         }
-
     }
-
 }
 
 @Composable
-private fun ParkMapView(assets: List<Asset>) {
+private fun ParkMapView(assets: List<Asset>, parkPosition: LatLng) {
     val mapView = rememberMapViewWithLifecycle()
-    MapViewContainer(mapView, assets = assets)
+    MapViewContainer(mapView, assets = assets, parkPosition = parkPosition)
 }
 
 @Composable
 private fun MapViewContainer(
     map: MapView,
+    parkPosition: LatLng,
     assets: List<Asset>
 ) {
     var mapInitialized by remember(map) { mutableStateOf(false) }
+    val topPadding = with(LocalDensity.current) { 50.dp.toPx().toInt() }
+    val bottomPadding = with(LocalDensity.current) { 50.dp.toPx().toInt() }
+    val cameraPadding = with(LocalDensity.current) { 30.dp.toPx().toInt() }
     LaunchedEffect(map, mapInitialized) {
         if (!mapInitialized) {
             val googleMap = map.awaitMap()
-            val latLngBoundsBuilder = LatLngBounds.Builder()
-            assets
-                .forEach { asset ->
-                    val position = LatLng(asset.latitude!!, asset.longitude!!)
-                    googleMap.addMarker {
-                        position(position)
-                        title(asset.title)
-                        snippet(asset.subtype)
+            googleMap.setPadding(0, topPadding, 0, bottomPadding)
+            if (assets.isNotEmpty()) {
+                val latLngBoundsBuilder = LatLngBounds.Builder()
+                assets
+                    .forEach { asset ->
+                        val position = LatLng(asset.latitude!!, asset.longitude!!)
+                        googleMap.addMarker {
+                            position(position)
+                            title(asset.title)
+                            snippet(asset.subtype)
+                        }
+                        latLngBoundsBuilder.include(position)
                     }
-                    latLngBoundsBuilder.include(position)
-                }
 
-            googleMap.moveCamera(CameraUpdateFactory.newLatLngBounds(
-                latLngBoundsBuilder.build(), 20))
+                googleMap.moveCamera(
+                    CameraUpdateFactory.newLatLngBounds(
+                        latLngBoundsBuilder.build(), cameraPadding
+                    )
+                )
+            } else {
+                googleMap.addMarker {
+                    position(parkPosition)
+                }
+                googleMap.moveCamera(
+                    CameraUpdateFactory.newLatLngZoom(parkPosition, 16F)
+                )
+            }
             mapInitialized = true
         }
     }
