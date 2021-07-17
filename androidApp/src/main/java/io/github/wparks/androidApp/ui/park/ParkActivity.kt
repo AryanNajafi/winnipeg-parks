ee4ca57ebc0b52b0108723aed01f4be1ae9a26f8
Aryan Najafi <aryannjfi@gmail.com>
2021-07-17 00:32:50 -0500

    Change default map markers to icon markers

--- androidApp/src/main/java/io/github/wparks/androidApp/ui/park/ParkActivity.kt
+++ androidApp/src/main/java/io/github/wparks/androidApp/ui/park/ParkActivity.kt
@@ -1,9 +1,12 @@
 package io.github.wparks.androidApp.ui.park
 
+import android.content.Context
+import android.graphics.Bitmap
 import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.content.res.AppCompatResources
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
@@ -18,9 +21,12 @@ import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.viewinterop.AndroidView
+import androidx.core.graphics.drawable.toBitmap
 import androidx.core.view.WindowCompat
 import com.google.accompanist.insets.LocalWindowInsets
 import com.google.accompanist.insets.ProvideWindowInsets
@@ -30,11 +36,13 @@ import com.google.accompanist.insets.ui.TopAppBar
 import com.google.accompanist.systemuicontroller.rememberSystemUiController
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.MapView
+import com.google.android.gms.maps.model.BitmapDescriptorFactory
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.LatLngBounds
 import com.google.maps.android.ktx.addMarker
 import com.google.maps.android.ktx.awaitMap
 import dagger.hilt.android.AndroidEntryPoint
+import io.github.wparks.androidApp.R
 import io.github.wparks.androidApp.ui.MyApp
 import io.github.wparks.androidApp.ui.home.HomeActivity
 import io.github.wparks.shared.Asset
@@ -149,6 +157,9 @@ private fun MapViewContainer(
     val topPadding = with(LocalDensity.current) { 50.dp.toPx().toInt() }
     val bottomPadding = with(LocalDensity.current) { 50.dp.toPx().toInt() }
     val cameraPadding = with(LocalDensity.current) { 30.dp.toPx().toInt() }
+
+    val context = LocalContext.current
+
     LaunchedEffect(map, mapInitialized) {
         if (!mapInitialized) {
             val googleMap = map.awaitMap()
@@ -162,6 +173,8 @@ private fun MapViewContainer(
                             position(position)
                             title(asset.title)
                             snippet(asset.subtype)
+                            icon(BitmapDescriptorFactory.fromBitmap(
+                                getMarkerIcon(context = context, assetId = asset.typeId)))
                         }
                         latLngBoundsBuilder.include(position)
                     }
@@ -186,6 +199,50 @@ private fun MapViewContainer(
     AndroidView({ map }) { mapView ->
 
     }
+}
+
+private fun getMarkerIcon(context: Context, assetId: Long): Bitmap {
+    val resId = when (assetId.toInt()) {
+        100 -> R.drawable.map_marker_seat
+        101 -> R.drawable.map_marker_sailing
+        102 -> R.drawable.map_marker_bike
+        103 -> R.drawable.map_marker_monuement
+        104 -> R.drawable.map_marker_grill
+        105 -> R.drawable.map_marker_shelter
+        106 -> R.drawable.map_marker_sledding
+        107 -> R.drawable.map_marker_parking
+        108 -> R.drawable.map_marker_park
+        109 -> R.drawable.map_marker_ice_skating
+        110 -> R.drawable.map_marker_skateboarding
+        111 -> R.drawable.map_marker_pets
+        112 -> R.drawable.map_marker_park
+        113 -> R.drawable.map_marker_park
+        114 -> R.drawable.map_marker_park
+        115 -> R.drawable.map_marker_fitness
+        116 -> R.drawable.map_marker_skiing
+        117 -> R.drawable.map_marker_soccer
+        118 -> R.drawable.map_marker_football
+        119 -> R.drawable.map_marker_baseball
+        120 -> R.drawable.map_marker_rugby
+        121 -> R.drawable.map_marker_circle
+        122 -> R.drawable.map_marker_sports
+        123 -> R.drawable.map_marker_sports
+        124 -> R.drawable.map_marker_sports
+        125 -> R.drawable.map_marker_cricket
+        126 -> R.drawable.map_marker_sports
+        127 -> R.drawable.map_marker_handball
+        128 -> R.drawable.map_marker_basketball
+        129 -> R.drawable.map_marker_tennis
+        130 -> R.drawable.map_marker_tennis
+        131 -> R.drawable.map_marker_volleyball
+        132 -> R.drawable.map_marker_football
+        133 -> R.drawable.map_marker_sports
+        134 -> R.drawable.map_marker_running
+        135 -> R.drawable.map_marker_sports
+        136 -> R.drawable.map_marker_sports
+        else -> R.drawable.map_marker_park
+    }
+    return requireNotNull(AppCompatResources.getDrawable(context, resId)).toBitmap()
 }
 
 data class AssetSelector(val typeId: Long, val subType: String?, val size: String?)
